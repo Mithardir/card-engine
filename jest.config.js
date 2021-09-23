ce353259f1b5ce3ad6c71252e5d0ba978d9d1c4b
Mithardir <Mithardir@users.noreply.github.com>
2021-09-23 23:06:39 +0200

    tests

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// jest.config.js
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true,
+};
+
+module.exports = config;
+
+// Or async function
+module.exports = async () => {
+  return {
+    verbose: true,
+    moduleNameMapper: {
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        "<rootDir>/__mocks__/fileMock.js",
+    },
+  };
+};
